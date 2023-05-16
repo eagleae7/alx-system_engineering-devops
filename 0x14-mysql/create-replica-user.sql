8a4965c4a147b423ca3bd28cd5685f71b8d161a5
eagleae7 <spotarinz@gmail.com>
2023-05-16 22:15:13 +0100

    update

--- 0x14-mysql/create-replica-user.sql
+++
@@ -1,8 +0,0 @@
--- Create user to check DBstatus 
--- Run on the master server
-
-CREATE USER IF NOT EXISTS 'replica_user'@'%'
-IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT  REPLICATION CLIENT, SELECT ON *.*
-TO 'replica_user'@'%';
-FLUSH PRIVILEGES;
